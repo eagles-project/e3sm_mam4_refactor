E3SM-Project/E3SM

Add Sphagnum simulation capability (currently hard-coded for PFT 12)

components/clm/src/biogeochem/VegStructUpdateMod.F90
@@ -18,6 +18,7 @@ module VegStructUpdateMod
   use CanopyStateType      , only : canopystate_type
   use VegetationType            , only : veg_pp
   use CropType             , only : crop_type
+  use SoilHydrologyType    , only : soilhydrology_type 
   !
   implicit none
   save
@@ -32,7 +33,7 @@ contains
   !-----------------------------------------------------------------------
   subroutine VegStructUpdate(num_soilp, filter_soilp, &
        waterstate_vars, frictionvel_vars, dgvs_vars, cnstate_vars, &
-       carbonstate_vars, canopystate_vars, crop_vars)
+       carbonstate_vars, canopystate_vars, crop_vars, soilhydrology_vars)
     !
     ! !DESCRIPTION:
     ! On the radiation time step, use C state variables and epc to diagnose
@@ -41,6 +42,9 @@ contains
     ! !USES:
     use pftvarcon        , only : noveg, nc3crop, nc3irrig, nbrdlf_evr_shrub, nbrdlf_dcd_brl_shrub
     use pftvarcon        , only : ncorn, ncornirrig, npcropmin, ztopmx, laimx
+#if (defined HUM_HOL)
+    use pftvarcon        , only : humhol_ht
+#endif
     use clm_time_manager , only : get_rad_step_size
     use clm_varctl       , only : spinup_state, spinup_mortality_factor
     !
@@ -54,6 +58,7 @@ contains
     type(carbonstate_type) , intent(in)    :: carbonstate_vars
     type(canopystate_type) , intent(inout) :: canopystate_vars
     type(crop_type)        , intent(inout) :: crop_vars
+    type(soilhydrology_type) , intent(in)    :: soilhydrology_vars
     !
     ! !REVISION HISTORY:
     ! 10/28/03: Created by Peter Thornton
@@ -71,6 +76,7 @@ contains
     real(r8) :: tsai_min   ! PATCH derived minimum tsai
     real(r8) :: tsai_alpha ! monthly decay rate of tsai
     real(r8) :: dt         ! radiation time step (sec)
+    real(r8) :: fb1, fb2, thiswtht
 
     real(r8), parameter :: dtsmonth = 2592000._r8 ! number of seconds in a 30 day month (60x60x24x30)
     !-----------------------------------------------------------------------
@@ -100,7 +106,8 @@ contains
          fpcgrid            =>  dgvs_vars%fpcgrid_patch           ,       & ! Input:  [real(r8) (:) ] fractional area of pft (pft area/nat veg area)    
 
          snow_depth         =>  waterstate_vars%snow_depth_col    ,       & ! Input:  [real(r8) (:) ] snow height (m)                                   
-
+         zwt                =>  soilhydrology_vars%zwt_col        ,       &
+         h2osfc             =>  waterstate_vars%h2osfc_col        ,       &
          forc_hgt_u_patch   =>  frictionvel_vars%forc_hgt_u_patch ,       & ! Input:  [real(r8) (:) ] observational height of wind at pft-level [m]     
 
          leafc              =>  carbonstate_vars%leafc_patch      ,       & ! Input:  [real(r8) (:) ] (gC/m2) leaf C                                    
@@ -275,8 +282,21 @@ contains
             ol = min( max(snow_depth(c)-hbot(p), 0._r8), htop(p)-hbot(p))
             fb = 1._r8 - ol / max(1.e-06_r8, htop(p)-hbot(p))
          else
-            fb = 1._r8 - max(min(snow_depth(c),0.2_r8),0._r8)/0.2_r8   ! 0.2m is assumed
-            !depth of snow required for complete burial of grasses
+#if (defined HUM_HOL)
+           if (ivt(p) == 12) then
+             thiswtht = zwt(c)*-1.0_r8+humhol_ht/2.0_r8+h2osfc(c)/1000._r8 !height above hollow bottom
+             !calculate submerged LAI
+             !Calculate LAI buried by snow (5cm is assumed)
+             fb1 = 1._r8 - max(min(snow_depth(c),0.05_r8),0._r8)/0.05_r8
+             fb2 = 1._r8 - max(min(thiswtht,0.2_r8),0._r8)/0.2_r8 
+             fb = min(fb1, fb2)
+           else
+             fb = 1._r8 - max(min(snow_depth(c),0.2_r8),0._r8)/0.2_r8 ! 0.2m is assumed
+           end if
+#else
+           !depth of snow required for complete burial of grasses
+           fb = 1._r8 - max(min(snow_depth(c),0.2_r8),0._r8)/0.2_r8   !0.2m is assumed
+#endif
          endif
 
          elai(p) = max(tlai(p)*fb, 0.0_r8)

components/clm/src/biogeophys/CanopyFluxesMod.F90
@@ -406,6 +406,8 @@ contains
          rh_ref2m             => waterstate_vars%rh_ref2m_patch            , & ! Output: [real(r8) (:)   ]  2 m height surface relative humidity (%)                              
          rhaf                 => waterstate_vars%rh_af_patch               , & ! Output: [real(r8) (:)   ]  fractional humidity of canopy air [dimensionless]                     
 
+         h2o_moss_inter       => waterstate_vars%h2o_moss_inter_patch      , & ! Output: [real(r8) (:)   ]  Internal Moss water content
+         h2o_moss_wc          => waterstate_vars%h2o_moss_wc_patch         , & ! Output: [real(r8) (:)   ]  Total Moss water content
          n_irrig_steps_left   => waterflux_vars%n_irrig_steps_left_patch   , & ! Output: [integer  (:)   ]  number of time steps for which we still need to irrigate today              
          irrig_rate           => waterflux_vars%irrig_rate_patch           , & ! Output: [real(r8) (:)   ]  current irrigation rate [mm/s]                                        
          qflx_tran_veg        => waterflux_vars%qflx_tran_veg_patch        , & ! Output: [real(r8) (:)   ]  vegetation transpiration (mm H2O/s) (+ = to atm)                      
@@ -436,6 +438,7 @@ contains
          eflx_sh_soil         => energyflux_vars%eflx_sh_soil_patch        , & ! Output: [real(r8) (:)   ]  sensible heat flux from soil (W/m**2) [+ to atm]                      
          eflx_sh_veg          => energyflux_vars%eflx_sh_veg_patch         , & ! Output: [real(r8) (:)   ]  sensible heat flux from leaves (W/m**2) [+ to atm]                    
          eflx_sh_grnd         => energyflux_vars%eflx_sh_grnd_patch        , & ! Output: [real(r8) (:)   ]  sensible heat flux from ground (W/m**2) [+ to atm]
+         leafn                => nitrogenstate_vars%leafn_patch            , &
          begp                 => bounds%begp                               , &
          endp                 => bounds%endp                                 &
          )
@@ -825,6 +828,28 @@ contains
             svpts(p) = el(p)                         ! pa
             eah(p) = forc_pbot(t) * qaf(p) / 0.622_r8   ! pa
             rhaf(p) = eah(p)/svpts(p)
+
+            ! XShi 11/20/15 - Calculate the internal water (tissue water
+            ! content) content
+            ! for moss
+            if (veg_pp%itype(p) == 12) then
+                !DMRicciuto 12/4/2015 - changed to use average of layer 3 and 4 
+                h2o_moss_inter(p) = -18032 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)**4 +  &
+                                7248.1 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)**3 -  &
+                                591.74 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)**2 +  &
+                                6.9031 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)       &
+                                + 0.4945
+                if ((h2osoi_vol(c,3) + h2osoi_vol(c,4))/2._r8 .gt. 0.25) then
+                    h2o_moss_inter(p) = -18032 *0.25**4 + 7248.1 * 0.25**3 &
+                                     -591.74 *0.25**2 + 6.9031*0.25 + 0.4954
+                endif
+                if (leafn(p).gt. 0._r8) then
+                   h2o_moss_wc(p) = h2o_moss_inter(p) + h2ocan(p)/(leafn(p)*60.0*2.0_r8/1000.0_r8)
+                else
+                   h2o_moss_wc(p) = 0._r8
+                endif
+            end if
+
          end do
 
          ! Modification for shrubs proposed by X.D.Z 
@@ -863,7 +888,7 @@ contains
             call Photosynthesis (bounds, fn, filterp, &
                  svpts(begp:endp), eah(begp:endp), o2(begp:endp), co2(begp:endp), rb(begp:endp), btran(begp:endp), &
                  dayl_factor(begp:endp), atm2lnd_vars, temperature_vars, surfalb_vars, solarabs_vars, &
-                 canopystate_vars, photosyns_vars, nitrogenstate_vars, phosphorusstate_vars, phase='sun')
+                 canopystate_vars, photosyns_vars, nitrogenstate_vars, phosphorusstate_vars, waterstate_vars, phase='sun')
 
             if ( use_c13 ) then
                call Fractionation (bounds, fn, filterp, &
@@ -887,7 +912,7 @@ contains
             call Photosynthesis (bounds, fn, filterp, &
                  svpts(begp:endp), eah(begp:endp), o2(begp:endp), co2(begp:endp), rb(begp:endp), btran(begp:endp), &
                  dayl_factor(begp:endp), atm2lnd_vars, temperature_vars, surfalb_vars, solarabs_vars, &
-                 canopystate_vars, photosyns_vars, nitrogenstate_vars, phosphorusstate_vars, phase='sha')
+                 canopystate_vars, photosyns_vars, nitrogenstate_vars, phosphorusstate_vars, waterstate_vars, phase='sha')
 
             if ( use_c13 ) then
                call Fractionation (bounds, fn, filterp,  &

eagles-project/e3sm_mam4_refactor

Conflicts:
	components/cam/src/physics/rrtmgp/external
	components/clm/src/biogeochem/VegStructUpdateMod.F90
	components/clm/src/biogeophys/BalanceCheckMod.F90
	components/clm/src/biogeophys/CanopyFluxesMod.F90
	components/clm/src/biogeophys/PhotosynthesisMod.F90
	components/clm/src/biogeophys/WaterStateType.F90
	components/clm/src/main/clm_driver.F90
	components/clm/src/main/pftvarcon.F90

components/elm/src/biogeochem/VegStructUpdateMod.F90
@@ -40,6 +40,9 @@ contains
     use pftvarcon        , only : noveg, nc3crop, nc3irrig, nbrdlf_evr_shrub, nbrdlf_dcd_brl_shrub
     use pftvarcon        , only : ncorn, ncornirrig, npcropmin, ztopmx, laimx
     use pftvarcon        , only : nmiscanthus, nmiscanthusirrig, nswitchgrass, nswitchgrassirrig
+#if (defined HUM_HOL)
+    use pftvarcon        , only : humhol_ht
+#endif
     use clm_time_manager , only : get_rad_step_size
     use elm_varctl       , only : spinup_state, spinup_mortality_factor
     !
@@ -67,6 +70,7 @@ contains
     real(r8) :: tsai_min   ! PATCH derived minimum tsai
     real(r8) :: tsai_alpha ! monthly decay rate of tsai
     real(r8) :: dt         ! radiation time step (sec)
+    real(r8) :: fb1, fb2, thiswtht
 
     real(r8), parameter :: dtsmonth = 2592000._r8 ! number of seconds in a 30 day month (60x60x24x30)
     !-----------------------------------------------------------------------
@@ -90,8 +94,10 @@ contains
          displar            =>  veg_vp%displar                ,       & ! Input:  [real(r8) (:) ] ratio of displacement height to canopy top height (-)
          dwood              =>  veg_vp%dwood                  ,       & ! Input:  [real(r8) (:) ] density of wood (gC/m^3)
 
-         snow_depth         =>  col_ws%snow_depth    ,       & ! Input:  [real(r8) (:) ] snow height (m)
+         snow_depth         =>  col_ws%snow_depth             ,       & ! Input:  [real(r8) (:) ] snow height (m)
 
+         zwt                =>  col_ws%zwt_col                ,       &
+         h2osfc             =>  col_ws%h2osfc_col             ,       &
          forc_hgt_u_patch   =>  frictionvel_vars%forc_hgt_u_patch ,       & ! Input:  [real(r8) (:) ] observational height of wind at pft-level [m]
 
          leafc              =>  veg_cs%leafc      ,       & ! Input:  [real(r8) (:) ] (gC/m2) leaf C
@@ -252,8 +258,21 @@ contains
             ol = min( max(snow_depth(c)-hbot(p), 0._r8), htop(p)-hbot(p))
             fb = 1._r8 - ol / max(1.e-06_r8, htop(p)-hbot(p))
          else
-            fb = 1._r8 - max(min(snow_depth(c),0.2_r8),0._r8)/0.2_r8   ! 0.2m is assumed
-            !depth of snow required for complete burial of grasses
+#if (defined HUM_HOL)
+           if (ivt(p) == 12) then
+             thiswtht = zwt(c)*-1.0_r8+humhol_ht/2.0_r8+h2osfc(c)/1000._r8 !height above hollow bottom
+             !calculate submerged LAI
+             !Calculate LAI buried by snow (5cm is assumed)
+             fb1 = 1._r8 - max(min(snow_depth(c),0.05_r8),0._r8)/0.05_r8
+             fb2 = 1._r8 - max(min(thiswtht,0.2_r8),0._r8)/0.2_r8 
+             fb = min(fb1, fb2)
+           else
+             fb = 1._r8 - max(min(snow_depth(c),0.2_r8),0._r8)/0.2_r8 ! 0.2m is assumed
+           end if
+#else
+           !depth of snow required for complete burial of grasses
+           fb = 1._r8 - max(min(snow_depth(c),0.2_r8),0._r8)/0.2_r8   !0.2m is assumed
+#endif
          endif
 
          elai(p) = max(tlai(p)*fb, 0.0_r8)

components/elm/src/biogeophys/CanopyFluxesMod.F90
@@ -408,6 +408,9 @@ contains
          rh_ref2m             => veg_ws%rh_ref2m            , & ! Output: [real(r8) (:)   ]  2 m height surface relative humidity (%)
          rhaf                 => veg_ws%rh_af               , & ! Output: [real(r8) (:)   ]  fractional humidity of canopy air [dimensionless]
 
+         h2o_moss_inter       => veg_ws%h2o_moss_inter_patch , & ! Output: [real(r8) (:)   ]  Internal Moss water content
+         h2o_moss_wc          => veg_ws%h2o_moss_wc_patch    , & ! Output: [real(r8) (:)   ]  Total Moss water content
+
          !pgwgt                => veg_pp%wtgcell              , & ! Input:  [integer  (:)   ]  pft's weight in gridcell
          n_irrig_steps_left   => veg_wf%n_irrig_steps_left   , & ! Output: [integer  (:)   ]  number of time steps for which we still need to irrigate today
          irrig_rate           => veg_wf%irrig_rate           , & ! Output: [real(r8) (:)   ]  current irrigation rate [mm/s]
@@ -439,6 +442,8 @@ contains
          eflx_sh_soil         => veg_ef%eflx_sh_soil        , & ! Output: [real(r8) (:)   ]  sensible heat flux from soil (W/m**2) [+ to atm]
          eflx_sh_veg          => veg_ef%eflx_sh_veg         , & ! Output: [real(r8) (:)   ]  sensible heat flux from leaves (W/m**2) [+ to atm]
          eflx_sh_grnd         => veg_ef%eflx_sh_grnd        , & ! Output: [real(r8) (:)   ]  sensible heat flux from ground (W/m**2) [+ to atm]
+         leafn                => veg_ns%leafn_patch         , &
+
          begp                 => bounds%begp                               , &
          endp                 => bounds%endp                                 &
          )
@@ -845,6 +850,28 @@ contains
             svpts(p) = el(p)                         ! pa
             eah(p) = forc_pbot(t) * qaf(p) / 0.622_r8   ! pa
             rhaf(p) = eah(p)/svpts(p)
+
+            ! XShi 11/20/15 - Calculate the internal water (tissue water
+            ! content) content
+            ! for moss
+            if (veg_pp%itype(p) == 12) then
+                !DMRicciuto 12/4/2015 - changed to use average of layer 3 and 4 
+                h2o_moss_inter(p) = -18032 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)**4 +  &
+                                7248.1 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)**3 -  &
+                                591.74 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)**2 +  &
+                                6.9031 * ((h2osoi_vol(c,3)+h2osoi_vol(c,4))/2._r8)       &
+                                + 0.4945
+                if ((h2osoi_vol(c,3) + h2osoi_vol(c,4))/2._r8 .gt. 0.25) then
+                    h2o_moss_inter(p) = -18032 *0.25**4 + 7248.1 * 0.25**3 &
+                                     -591.74 *0.25**2 + 6.9031*0.25 + 0.4954
+                endif
+                if (leafn(p).gt. 0._r8) then
+                   h2o_moss_wc(p) = h2o_moss_inter(p) + h2ocan(p)/(leafn(p)*60.0*2.0_r8/1000.0_r8)
+                else
+                   h2o_moss_wc(p) = 0._r8
+                endif
+            end if
+
          end do
 
          ! Modification for shrubs proposed by X.D.Z