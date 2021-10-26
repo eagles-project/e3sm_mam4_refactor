9879ef817041adbf3d6e0938c57c3064a22497de
Luca Bertagna <lbertag@sandia.gov>
2021-10-26 13:28:26 -0600

    HOMMEXX: changes to Kokkos.cmake
    These mods allow a host project to build kokkos, without homme
    assuming where the kokkos targets are built. Before this change,
    homme *assumed* the location of include/lib dirs of an already
    built kokkos. Now, it only does that if E3SM_KOKKOS_PATH is provided.
    If E3SM_KOKKOS_ALREADY_BUILT, it instead verifies that the kokkos
    libs are valid cmake targets, and then links kokkos as a cmake target,
    letting cmake propagate include/lib dirs, flags, defs,...

--- components/homme/cmake/Kokkos.cmake
+++ components/homme/cmake/Kokkos.cmake
@@ -1,5 +1,27 @@
+SET (KOKKOS_LIBRARIES "kokkoscore;kokkoscontainers")
+SET (KOKKOS_TPL_LIBRARIES "dl")
+
 set (KOKKOS_INSTALLATION_NEEDED FALSE CACHE BOOL "")
-if (NOT DEFINED E3SM_KOKKOS_PATH)
+if (DEFINED E3SM_KOKKOS_PATH)
+  message ("STATUS The E3SM installation of kokkos in ${E3SM_KOKKOS_PATH} will be used. Here are the details:")
+  SET (KOKKOS_INCLUDE_DIR ${E3SM_KOKKOS_PATH}/include)
+  SET (KOKKOS_LIBRARY_DIR ${E3SM_KOKKOS_PATH}/lib64)
+
+  MESSAGE ("    KOKKOS_INCLUDE_DIR: ${KOKKOS_INCLUDE_DIR}")
+  MESSAGE ("    KOKKOS_LIBRARY_DIR: ${KOKKOS_LIBRARY_DIR}")
+  MESSAGE ("    KOKKOS_LIBRARIES:   ${KOKKOS_LIBRARIES}")
+  set (KOKKOS_LIBS_ARE_TARGETS FALSE CACHE BOOL "" INTERNAL)
+elseif (E3SM_INTERNAL_KOKKOS_ALREADY_BUILT)
+  # Check that all the needed kokkos libs are also valid targets
+  # built within thi CMake project
+  foreach (lib IN_LIST KOKKOS_LIBRARIES)
+    if (NOT TARGET ${lib})
+      message (FATAL_ERROR "Error! E3SM_INTERNAL_KOKKOS_ALREADY_BUILT=TRUE, but ${lib} is not a cmake TARGET.\n")
+    endif()
+  endforeach()
+  message (STATUS "All kokkos libraries needed by Homme are already built by this cmake project.\n")
+  set (KOKKOS_LIBS_ARE_TARGETS TRUE CACHE BOOL "" INTERNAL)
+else()
   # Build kokkos submodule if user did not specify KOKKOS_PATH.
   set (KOKKOS_SRC ${CMAKE_SOURCE_DIR}/../../externals/kokkos)
   set (KOKKOS_BUILD_DIR ${CMAKE_BINARY_DIR}/kokkos/build)
@@ -13,26 +35,16 @@ if (NOT DEFINED E3SM_KOKKOS_PATH)
        ${KOKKOS_SRC}/algorithms/src
        ${KOKKOS_BUILD_DIR})
   SET (KOKKOS_LIBRARY_DIR ${KOKKOS_BUILD_DIR})
-  message ("Using Kokkos built internally to E3SM, in ${KOKKOS_BUILD_DIR}")
-  if (NOT E3SM_INTERNAL_KOKKOS_ALREADY_BUILT)
-    # Nobody else already added kokkos subdirectory, so we will do it
-    set (KOKKOS_INSTALLATION_NEEDED TRUE)
-    set (E3SM_INTERNAL_KOKKOS_ALREADY_BUILT TRUE)
-  endif()
-
-else ()
-  message ("The installation of kokkos in ${E3SM_KOKKOS_PATH} will be used.")
-  SET (KOKKOS_INCLUDE_DIR ${E3SM_KOKKOS_PATH}/include)
-  SET (KOKKOS_LIBRARY_DIR ${E3SM_KOKKOS_PATH}/lib64)
+  string (CONCAT msg
+    "HOMME: building Kokkos internally, with\n"
+    "  KOKKOS_SOURCE_DIR: ${KOKKOS_SRC}\n"
+    "  KOKKOS_BINARY_DIR: ${KOKKOS_BUILD_DIR}\n")
+  message (STATUS "${msg}")
+  # Nobody else already added kokkos subdirectory, so we will do it
+  set (KOKKOS_INSTALLATION_NEEDED TRUE)
+  set (E3SM_INTERNAL_KOKKOS_ALREADY_BUILT TRUE)
+  set (KOKKOS_LIBS_ARE_TARGETS TRUE CACHE BOOL "" INTERNAL)
 endif ()
-
-SET (KOKKOS_LIBRARIES "kokkoscore;kokkoscontainers")
-SET (KOKKOS_TPL_LIBRARIES "dl")
-
-MESSAGE (STATUS "Kokkos installation found! Here are the details:")
-MESSAGE ("    KOKKOS_INCLUDE_DIR: ${KOKKOS_INCLUDE_DIR}")
-MESSAGE ("    KOKKOS_LIBRARY_DIR: ${KOKKOS_LIBRARY_DIR}")
-MESSAGE ("    KOKKOS_LIBRARIES:   ${KOKKOS_LIBRARIES}")
 
 macro(install_kokkos_if_needed)
   if (KOKKOS_INSTALLATION_NEEDED)
@@ -44,6 +56,10 @@ macro(install_kokkos_if_needed)
 endmacro()
 
 macro(link_to_kokkos targetName)
-  TARGET_INCLUDE_DIRECTORIES(${targetName} SYSTEM PUBLIC ${KOKKOS_INCLUDE_DIR})
-  TARGET_LINK_LIBRARIES(${targetName} ${KOKKOS_TPL_LIBRARIES} ${KOKKOS_LIBRARIES} -L${KOKKOS_LIBRARY_DIR})
+  if (KOKKOS_LIBS_ARE_TARGETS)
+    TARGET_LINK_LIBRARIES (${targetName} ${KOKKOS_LIBRARIES})
+  else()
+    TARGET_INCLUDE_DIRECTORIES(${targetName} SYSTEM PUBLIC ${KOKKOS_INCLUDE_DIR})
+    TARGET_LINK_LIBRARIES(${targetName} ${KOKKOS_TPL_LIBRARIES} ${KOKKOS_LIBRARIES} -L${KOKKOS_LIBRARY_DIR})
+  endif()
 endmacro(link_to_kokkos)
