6cbe3cfc180cb85cdda92f015a7a19d38e97cec0
dqwu <wuda@mcs.anl.gov>
2021-10-26 13:26:10 -0500

    Adding required ADIOS2 libs to cmake_macros files (in progress)

--- cime_config/machines/cmake_macros/pgigpu_summit.cmake
+++ cime_config/machines/cmake_macros/pgigpu_summit.cmake
@@ -1,3 +1,6 @@
 string(APPEND LDFLAGS " -gpu=cc70,cc60 -Minfo=accel")
 string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf -L$ENV{HDF5_PATH}/lib -lhdf5_hl -lhdf5 -L$ENV{NETCDF_C_PATH}/lib -lnetcdf -L$ENV{NETCDF_FORTRAN_PATH}/lib -lnetcdff -L$ENV{ESSL_PATH}/lib64 -lessl -L$ENV{OLCF_NETLIB_LAPACK_ROOT}/lib64 -llapack")
+if (NOT MPILIB STREQUAL mpi-serial)
+  string(APPEND SLIBS " -L$ENV{ADIOS2_DIR}/lib64 -ladios2_c_mpi -ladios2_c -ladios2_core_mpi -ladios2_core -ladios2_evpath -ladios2_ffs -ladios2_dill -ladios2_atl -ladios2_enet")
+endif()
 set(KOKKOS_OPTIONS "--arch=Power9,Volta70 --with-cuda=$ENV{CUDA_DIR} --with-cuda-options=enable_lambda")
