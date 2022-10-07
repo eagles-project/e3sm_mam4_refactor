ff3a048f8b6d7d32ff50063861aa37e62e7d6dbe
Shuaiqi Tang <shuaiqi.tang@pnnl.gov>
2022-10-07 11:38:51 -0700

    remove mam_prevap_resusp_optaa from input list of modal_aero_convproc.F90 and ma_convproc_intr in aero_model.F90

--- components/eam/src/chemistry/modal_aero/aero_model.F90
+++ components/eam/src/chemistry/modal_aero/aero_model.F90
@@ -2262,7 +2262,7 @@ do_lphase2_conditional: &
             dlf, dlf2, cmfmc2, sh_e_ed_ratio,                           &
             nsrflx_mzaer2cnvpr, qsrflx_mzaer2cnvpr, aerdepwetis,        &
             mu, md, du, eu, ed, dp, dsubcld, jt, maxg, ideep, lengath,  &
-            species_class, mam_prevap_resusp_optaa,                     &
+            species_class,                                              &
             history_aero_prevap_resusp                                  )
        call t_stopf('ma_convproc')       
     endif
