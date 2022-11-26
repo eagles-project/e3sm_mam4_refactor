e6fa8d3fe06fb67988b322a25279c07aa359d659
Hui Wan <hui.wan@pnnl.gov>
2022-11-26 02:27:25 -0800

    amicphys_intr: new subr. accumulate_column_tend_integrals

--- components/eam/src/chemistry/modal_aero/modal_aero_amicphys_diags.F90
+++ components/eam/src/chemistry/modal_aero/modal_aero_amicphys_diags.F90
@@ -1,9 +1,5 @@
 module modal_aero_amicphys_diags
 
-  use modal_aero_amicphys_control, only: do_q_coltendaa, do_qqcw_coltendaa, &
-                                          iqtend_cond, &
-                                          iqtend_rnam, iqqcwtend_rnam, &
-                                          iqtend_nnuc, iqtend_coag
 !                                         suffix_q_coltendaa, suffix_qqcw_coltendaa
   implicit none
 
@@ -15,6 +11,10 @@ subroutine amicphys_diags_init( do_cond, do_rename, do_newnuc, do_coag )
 !-----------------------------------------------------------------------------------
 ! Purpose: set switches that turn on or off column-integrated tendency diagnostics.
 !-----------------------------------------------------------------------------------
+  use modal_aero_amicphys_control, only: do_q_coltendaa, do_qqcw_coltendaa, &
+                                         iqtend_cond, iqtend_rnam, iqtend_nnuc, iqtend_coag, &
+                                         iqqcwtend_rnam
+
   logical,intent(in) :: do_cond, do_rename, do_newnuc, do_coag
 
   if ( (.not. do_cond) .and. (.not. do_rename) ) then
@@ -69,5 +69,55 @@ subroutine get_gcm_tend_diags_from_subareas( nsubarea, ncldy_subarea, afracsub, 
   end if
 
 end subroutine get_gcm_tend_diags_from_subareas
+
+subroutine accumulate_column_tend_integrals( pdel, gravit,                &! in
+                                             qgcm_tendaa, qqcwgcm_tendaa, &! in
+                                             q_coltendaa, qqcw_coltendaa  )! inout
+!-------------------------------------------------------------------------------
+! Purpose: add mass-weighted tendencies to the corresponding vertical integrals
+!-------------------------------------------------------------------------------
+
+  use modal_aero_amicphys_control, only: wp=>r8, ncnst=>gas_pcnst
+  use modal_aero_amicphys_control, only: do_q_coltendaa, do_qqcw_coltendaa, &
+                                         nqtendaa, nqqcwtendaa
+
+  real(wp),intent(in) :: pdel       ! layer thickness in pressure coordinate [Pa]
+  real(wp),intent(in) :: gravit     ! gravitational acceleration [m/s^2]
+
+  real(wp),intent(in)    ::    qgcm_tendaa(ncnst,   nqtendaa)
+  real(wp),intent(in)    :: qqcwgcm_tendaa(ncnst,nqqcwtendaa)
+
+  real(wp),intent(inout) ::    q_coltendaa(ncnst,   nqtendaa)
+  real(wp),intent(inout) :: qqcw_coltendaa(ncnst,nqqcwtendaa)
+
+  real(wp) :: pdel_fac   ! = air density * dz
+
+  integer :: iqtend  ! loop index corresponding to different aerosol processes
+  integer :: icnst   ! loop index corresponding to different constituents (tracers)
+
+  pdel_fac = pdel/gravit
+
+  ! Gases and interstitial aerosols
+
+  do iqtend = 1,nqtendaa
+  do icnst = 1,ncnst
+     if ( do_q_coltendaa(icnst,iqtend) ) then
+        q_coltendaa(icnst,iqtend) = q_coltendaa(icnst,iqtend) + qgcm_tendaa(icnst,iqtend)*pdel_fac
+     end if
+  end do ! icnst
+  end do ! iqtend
+
+  ! cloud-borne aerosols
+
+  do iqtend = 1,nqqcwtendaa 
+  do icnst = 1,ncnst
+     if ( do_qqcw_coltendaa(icnst,iqtend) ) then
+        qqcw_coltendaa(icnst,iqtend) = qqcw_coltendaa(icnst,iqtend) + qqcwgcm_tendaa(icnst,iqtend)*pdel_fac
+     end if
+  end do ! icnst
+  end do ! iqtend
+
+end subroutine accumulate_column_tend_integrals
+
 
 end module modal_aero_amicphys_diags
