7b14772c7de49d2753e1b72c1ee3aecaaa163076
Hui Wan <hui.wan@pnnl.gov>
2023-02-03 12:13:43 -0800

    drydep: revise variable names in cfdotmc_pro

--- components/eam/src/chemistry/modal_aero/mo_spitfire_transport.F90
+++ components/eam/src/chemistry/modal_aero/mo_spitfire_transport.F90
@@ -215,169 +215,171 @@ contains
 
 
 !##############################################################################
+! Calculate the derivative for the interpolating polynomial.
+! Multi column version.
+!##############################################################################
 
-  subroutine cfdotmc_pro (ncol, x, f, fdot)
-
-!     prototype version; eventually replace with final SPITFIRE scheme
-
-!     calculate the derivative for the interpolating polynomial
-!     multi column version
-
+  subroutine cfdotmc_pro (ncol, xw, ff, fdot)
 
     implicit none
 
-! input
-    integer ncol                      ! number of colums to process
+    integer ncol                            ! number of colums to process
 
-    real (r8) x(pcols, pverp)
-    real (r8) f(pcols, pverp)
-! output
-    real (r8) fdot(pcols, pverp)          ! derivative at nodes
+    real(r8),intent(in)  ::   xw(pcols,pverp)   ! coordinate variable
+    real(r8),intent(in)  ::   ff(pcols,pverp)   ! value at notes 
+    real(r8),intent(out) :: fdot(pcols,pverp)   ! derivative at nodes
 
-! assumed variable distribution
-!     x1.......x2.......x3.......x4.......x5.......x6     1,pverp points
-!     f1.......f2.......f3.......f4.......f5.......f6     1,pverp points
+! Assumed variable distribution (staggering)
+!     xw1.....xw2......xw3......xw4......xw5.......xw6    1,pverp points
+!     ff1.....ff2......ff3......ff4......ff5.......ff6    1,pverp points
 !     ...sh1.......sh2......sh3......sh4......sh5....     1,pver points
-!     .........d2.......d3.......d4.......d5.........     2,pver points
-!     .........s2.......s3.......s4.......s5.........     2,pver points
+!     ........dd2......dd3......dd4......dd5.........     2,pver points
+!     ........ss2......ss3......ss4......ss5.........     2,pver points
 !     .............dh2......dh3......dh4.............     2,pver-1 points
 !     .............eh2......eh3......eh4.............     2,pver-1 points
-!     ..................e3.......e4..................     3,pver-1 points
+!     .................ee3.......ee4.................     3,pver-1 points
 !     .................ppl3......ppl4................     3,pver-1 points
 !     .................ppr3......ppr4................     3,pver-1 points
-!     .................t3........t4..................     3,pver-1 points
+!     .................tt3.......tt4.................     3,pver-1 points
 !     ................fdot3.....fdot4................     3,pver-1 points
 
 
-! work variables
+    integer ii
+    integer kk
 
-
-    integer i
-    integer k
-
-    real (r8) a                    ! work var
-    real (r8) b                    ! work var
-    real (r8) c                    ! work var
-    real (r8) s(pcols,pverp)             ! first divided differences at nodes
-    real (r8) sh(pcols,pverp)            ! first divided differences between nodes
-    real (r8) d(pcols,pverp)             ! second divided differences at nodes
-    real (r8) dh(pcols,pverp)            ! second divided differences between nodes
-    real (r8) e(pcols,pverp)             ! third divided differences at nodes
-    real (r8) eh(pcols,pverp)            ! third divided differences between nodes
+    real (r8) aa                   ! work var
+    real (r8) bb                   ! work var
+    real (r8) cc                   ! work var
+    real (r8) ss(pcols,pverp)      ! first divided differences at nodes
+    real (r8) sh(pcols,pverp)      ! first divided differences between nodes
+    real (r8) dd(pcols,pverp)      ! second divided differences at nodes
+    real (r8) dh(pcols,pverp)      ! second divided differences between nodes
+    real (r8) ee(pcols,pverp)      ! third divided differences at nodes
+    real (r8) eh(pcols,pverp)      ! third divided differences between nodes
     real (r8) pp                   ! p prime
-    real (r8) ppl(pcols,pverp)           ! p prime on left
-    real (r8) ppr(pcols,pverp)           ! p prime on right
+    real (r8) ppl(pcols,pverp)     ! p prime on left
+    real (r8) ppr(pcols,pverp)     ! p prime on right
     real (r8) qpl
     real (r8) qpr
-    real (r8) ttt
-    real (r8) t
+    real (r8) ttlmt
+    real (r8) tt
     real (r8) tmin
     real (r8) tmax
     real (r8) delxh(pcols,pverp)
 
+    ! The minmod function 
 
-!     the minmod function 
     real (r8) minmod
     real (r8) medan
-    minmod(a,b) = 0.5_r8*(sign(1._r8,a) + sign(1._r8,b))*min(abs(a),abs(b))
-    medan(a,b,c) = a + minmod(b-a,c-a)
 
-    do k = 1,pver
+    minmod(aa,bb) = 0.5_r8*(sign(1._r8,aa) + sign(1._r8,bb))*min(abs(aa),abs(bb))
+    medan(aa,bb,cc) = aa + minmod(bb-aa,cc-aa)
 
+    !-----------------
+    do kk = 1,pver
 
-!        first divided differences between nodes
-       do i = 1, ncol
-          delxh(i,k) = (x(i,k+1)-x(i,k))
-          sh(i,k) = (f(i,k+1)-f(i,k))/delxh(i,k)
+       ! First divided differences between nodes
+
+       do ii = 1, ncol
+          delxh(ii,kk) = xw(ii,kk+1) - xw(ii,kk)
+             sh(ii,kk) = (ff(ii,kk+1)-ff(ii,kk))/delxh(ii,kk)
        end do
 
-!        first and second divided differences at nodes
-       if (k.ge.2) then
-          do i = 1,ncol
-             d(i,k) = (sh(i,k)-sh(i,k-1))/(x(i,k+1)-x(i,k-1))
-             s(i,k) = minmod(sh(i,k),sh(i,k-1))
+       ! First and second divided differences at nodes
+
+       if (kk.ge.2) then
+          do ii = 1,ncol
+             dd(ii,kk) = (sh(ii,kk)-sh(ii,kk-1))/(xw(ii,kk+1)-xw(ii,kk-1))
+             ss(ii,kk) = minmod(sh(ii,kk),sh(ii,kk-1))
           end do
        endif
-    end do
-
-!     second and third divided diffs between nodes
-    do k = 2,pver-1
-       do i = 1, ncol
-          eh(i,k) = (d(i,k+1)-d(i,k))/(x(i,k+2)-x(i,k-1))
-          dh(i,k) = minmod(d(i,k),d(i,k+1))
-       end do
-    end do
-
-!     treat the boundaries
-    do i = 1,ncol
-       e(i,2) = eh(i,2)
-       e(i,pver) = eh(i,pver-1)
-!        outside level
-       fdot(i,1) = sh(i,1) - d(i,2)*delxh(i,1)  &
-            - eh(i,2)*delxh(i,1)*(x(i,1)-x(i,3))
-       fdot(i,1) = minmod(fdot(i,1),3*sh(i,1))
-       fdot(i,pverp) = sh(i,pver) + d(i,pver)*delxh(i,pver)  &
-            + eh(i,pver-1)*delxh(i,pver)*(x(i,pverp)-x(i,pver-1))
-       fdot(i,pverp) = minmod(fdot(i,pverp),3*sh(i,pver))
-!        one in from boundary
-       fdot(i,2) = sh(i,1) + d(i,2)*delxh(i,1) - eh(i,2)*delxh(i,1)*delxh(i,2)
-       fdot(i,2) = minmod(fdot(i,2),3*s(i,2))
-       fdot(i,pver) = sh(i,pver) - d(i,pver)*delxh(i,pver)   &
-            - eh(i,pver-1)*delxh(i,pver)*delxh(i,pver-1)
-       fdot(i,pver) = minmod(fdot(i,pver),3*s(i,pver))
-    end do
-
-
-    do k = 3,pver-1
-       do i = 1,ncol
-          e(i,k) = minmod(eh(i,k),eh(i,k-1))
-       end do
-    end do
-
-
-
-    do k = 3,pver-1
-
-       do i = 1,ncol
-
-!           p prime at k-0.5
-          ppl(i,k)=sh(i,k-1) + dh(i,k-1)*delxh(i,k-1)  
-!           p prime at k+0.5
-          ppr(i,k)=sh(i,k)   - dh(i,k)  *delxh(i,k)
-
-          t = minmod(ppl(i,k),ppr(i,k))
-
-!           derivate from parabola thru f(i,k-1), f(i,k), and f(i,k+1)
-          pp = sh(i,k-1) + d(i,k)*delxh(i,k-1) 
-
-!           quartic estimate of fdot
-          fdot(i,k) = pp                            &
-               - delxh(i,k-1)*delxh(i,k)            &
-               *(  eh(i,k-1)*(x(i,k+2)-x(i,k  ))    &
-               + eh(i,k  )*(x(i,k  )-x(i,k-2))      &
-               )/(x(i,k+2)-x(i,k-2))
-
-!           now limit it
-          qpl = sh(i,k-1)       &
-               + delxh(i,k-1)*minmod(d(i,k-1)+e(i,k-1)*(x(i,k)-x(i,k-2)), &
-               d(i,k)  -e(i,k)*delxh(i,k))
-          qpr = sh(i,k)         &
-               + delxh(i,k  )*minmod(d(i,k)  +e(i,k)*delxh(i,k-1),        &
-               d(i,k+1)+e(i,k+1)*(x(i,k)-x(i,k+2)))
-
-          fdot(i,k) = medan(fdot(i,k), qpl, qpr)
-
-          ttt = minmod(qpl, qpr)
-          tmin = min(0._r8,3*s(i,k),1.5_r8*t,ttt)
-          tmax = max(0._r8,3*s(i,k),1.5_r8*t,ttt)
-
-          fdot(i,k) = fdot(i,k) + minmod(tmin-fdot(i,k), tmax-fdot(i,k))
-
-       end do
 
     end do
 
-    return
+    ! Second and third divided diffs between nodes
+
+    do kk = 2,pver-1
+    do ii = 1,ncol
+          eh(ii,kk) = ( dd(ii,kk+1)-dd(ii,kk) )/( xw(ii,kk+2)-xw(ii,kk-1) )
+          dh(ii,kk) = minmod( dd(ii,kk), dd(ii,kk+1) )
+    end do
+    end do
+
+    ! Treat the boundaries
+
+    do ii = 1,ncol
+
+       ee(ii,2)    = eh(ii,2)
+       ee(ii,pver) = eh(ii,pver-1)
+
+       ! Outside level
+
+       fdot(ii,1) = sh(ii,1) - dd(ii,2)*delxh(ii,1) - eh(ii,2)*delxh(ii,1)*(xw(ii,1)-xw(ii,3))
+       fdot(ii,1) = minmod( fdot(ii,1), 3*sh(ii,1))
+
+       fdot(ii,pverp) = sh(ii,pver) + dd(ii,pver)*delxh(ii,pver)  &
+                      + eh(ii,pver-1)*delxh(ii,pver)*(xw(ii,pverp)-xw(ii,pver-1))
+       fdot(ii,pverp) = minmod( fdot(ii,pverp), 3*sh(ii,pver) )
+
+       ! One in from boundary
+
+       fdot(ii,2) = sh(ii,1) + dd(ii,2)*delxh(ii,1) - eh(ii,2)*delxh(ii,1)*delxh(ii,2)
+       fdot(ii,2) = minmod(fdot(ii,2),3*ss(ii,2))
+
+       fdot(ii,pver) = sh(ii,pver) - dd(ii,pver)*delxh(ii,pver)   &
+                     - eh(ii,pver-1)*delxh(ii,pver)*delxh(ii,pver-1)
+       fdot(ii,pver) = minmod( fdot(ii,pver), 3*ss(ii,pver) )
+
+    end do
+
+
+    do kk = 3,pver-1
+    do ii = 1,ncol
+       ee(ii,kk) = minmod( eh(ii,kk), eh(ii,kk-1) )
+    end do
+    end do
+
+
+    do kk = 3,pver-1
+    do ii = 1,ncol
+
+       ! p prime at k-0.5
+       ppl(ii,kk)=sh(ii,kk-1) + dh(ii,kk-1)*delxh(ii,kk-1)  
+
+       ! p prime at k+0.5
+       ppr(ii,kk)=sh(ii,kk)   - dh(ii,kk)  *delxh(ii,kk)
+
+       tt = minmod(ppl(ii,kk),ppr(ii,kk))
+
+       ! derivate from parabola thru f(i,k-1), f(i,k), and f(i,k+1)
+       pp = sh(ii,kk-1) + dd(ii,kk)*delxh(ii,kk-1) 
+
+       ! quartic estimate of fdot
+       fdot(ii,kk) = pp                                      &
+                   - delxh(ii,kk-1)*delxh(ii,kk)             &
+                   *(  eh(ii,kk-1)*(xw(ii,kk+2)-xw(ii,kk  )) &
+                     + eh(ii,kk  )*(xw(ii,kk  )-xw(ii,kk-2)) &
+                   )/(xw(ii,kk+2)-xw(ii,kk-2))
+
+       ! now limit it
+       qpl = sh(ii,kk-1)       &
+           + delxh(ii,kk-1)*minmod( dd(ii,kk-1)+ee(ii,kk-1)*(xw(ii,kk)-xw(ii,kk-2)), &
+                                    dd(ii,kk)  -ee(ii,kk  )*delxh(ii,kk)             )
+       qpr = sh(ii,kk)         &
+           + delxh(ii,kk  )*minmod(dd(ii,kk)  +ee(ii,kk)*delxh(ii,kk-1),        &
+                                   dd(ii,kk+1)+ee(ii,kk+1)*(xw(ii,kk)-xw(ii,kk+2)))
+
+       fdot(ii,kk) = medan( fdot(ii,kk), qpl, qpr )
+
+       ttlmt = minmod(qpl, qpr)
+       tmin = min(0._r8,3*ss(ii,kk),1.5_r8*tt,ttlmt)
+       tmax = max(0._r8,3*ss(ii,kk),1.5_r8*tt,ttlmt)
+
+       fdot(ii,kk) = fdot(ii,kk) + minmod( tmin-fdot(ii,kk), tmax-fdot(ii,kk))
+
+    end do
+    end do
+
   end subroutine cfdotmc_pro
 
 end module mo_spitfire_transport
