5459397698e5ecdaae56070f567d83d7a7257ad4
Shuaiqi Tang <shuaiqi.tang@pnnl.gov>
2023-06-14 11:14:39 -0700

    setext: remove unused code

--- components/eam/src/chemistry/mozart/mo_setext.F90
+++ components/eam/src/chemistry/mozart/mo_setext.F90
@@ -66,6 +66,7 @@ contains
 
   end subroutine setext_inti
 
+!==============================================================================
   subroutine setext( extfrc, zint_abs, zint_rel, cldtop, &
        zmid, lchnk, tfld, o2mmr, ommr, &
        pmid, mbar, rlats, calday, ncol, rlons, pbuf )
@@ -133,8 +134,6 @@ contains
 
     extfrc(:,:,:) = 0._r8
 
-    no_lgt(:,:) = 0._r8
-
     !--------------------------------------------------------
     !     ... set frcing from datasets
     !--------------------------------------------------------
@@ -143,101 +142,14 @@ contains
     !--------------------------------------------------------
     !     ... set nox production from lighting
     !         note: from ground to cloud top production is c shaped
+    !
+    ! FORTRAN refactor: nox is not included in current MAM4
+    ! the related code are removed but outfld is kept for BFB testing
     !--------------------------------------------------------
-    if ( no_ndx > 0 ) then
-       do i = 1,ncol
-          cldind = nint( cldtop(i) )
-          if( cldind < pver .and. cldind > 0  ) then
-             extfrc(i,cldind:pver,no_ndx) = extfrc(i,cldind:pver,no_ndx) &
-                  + prod_no(i,cldind:pver,lchnk)
-             no_lgt(i,cldind:pver) = prod_no(i,cldind:pver,lchnk)
-          end if
-       end do
-    endif
-    if ( xno_ndx > 0 ) then
-       do i = 1,ncol
-          cldind = nint( cldtop(i) )
-          if( cldind < pver .and. cldind > 0  ) then
-             extfrc(i,cldind:pver,xno_ndx) = extfrc(i,cldind:pver,xno_ndx) &
-                  + prod_no(i,cldind:pver,lchnk)
-          end if
-       end do
-    endif
-
+    no_lgt(:,:) = 0._r8
     call outfld( 'NO_Lightning', no_lgt(:ncol,:), ncol, lchnk )
 
     call airpl_set( lchnk, ncol, no_ndx, co_ndx, xno_ndx, cldtop, zint_abs, extfrc)
-
-    !---------------------------------------------------------------------
-    ! 	... synoz production
-    !---------------------------------------------------------------------
-    if( synoz_ndx > 0 ) then
-       do k = 1,pver
-          extfrc(:ncol,k,synoz_ndx) = extfrc(:ncol,k,synoz_ndx) + po3(:ncol,k,lchnk)
-       end do
-    end if
-
-    do i = 1,num_tracer_srcs
-
-       ndx =  get_extfrc_ndx( tracer_src_flds(i) )
-       call get_srcs_data( tracer_src_flds(i), srcs_offline,  ncol, lchnk, pbuf )
-       do k = 1,pver
-          extfrc(:ncol,k,ndx) = extfrc(:ncol,k,ndx) + srcs_offline(:ncol,k)
-       enddo
-
-    enddo
-
-
-    if ( has_ions ) then
-       !---------------------------------------------------------------------
-       !     ... set ion auroral production
-       !---------------------------------------------------------------------
-
-       call aurora( tfld, o2mmr, ommr, mbar, rlats, &
-            extfrc(:,:,o2p_ndx), extfrc(:,:,op_ndx), extfrc(:,:,n2p_ndx), extfrc(:,:,np_ndx), pmid, &
-            lchnk, calday, ncol, rlons, pbuf )
-       !---------------------------------------------------------------------
-       !     ... set n(2d) and n(4s) auroral production
-       !         Stan Solomon HAO
-       !         include production of N by secondary auroral hot electrons (e_s*):
-       !         this is not a "real" reaction; instead, the production is parameterized in terms
-       !         of the production rate of N2+ by primary electrons, QN2P (which is in the model),
-       !         as follows:
-       !---------------------------------------------------------------------
-       do k = 1,pver
-          extfrc(:,k,n2d_ndx) = 1.57_r8*.6_r8*extfrc(:,k,n2p_ndx)
-          extfrc(:,k,n_ndx) = 1.57_r8*.4_r8*extfrc(:,k,n2p_ndx)
-       end do
-       !---------------------------------------------------------------------
-       !     ... set electron auroral production
-       !---------------------------------------------------------------------
-       do k = 1,pver
-          extfrc(:,k,e_ndx) = extfrc(:,k,op_ndx) + extfrc(:,k,o2p_ndx) &
-               + extfrc(:,k,np_ndx) + extfrc(:,k,n2p_ndx)
-       end do
-       !---------------------------------------------------------------------
-       !     ... set SPE NOx and HOx production
-       !     Jackman et al., JGR, 2005 
-       !     production of 1.25 Nitrogen atoms/ion pair with branching ratios 
-       !     of 0.55 N(4S) and 0.7 N(2D).
-       !---------------------------------------------------------------------
-       call spe_prod( spe_nox, spe_hox, pmid, zmid, lchnk, ncol)
-
-       call outfld( 'N2D_SPE', 0.7_r8*spe_nox,  ncol, lchnk ) ! N(2D) produciton (molec/cm3/s)
-       call outfld( 'N4S_SPE',0.55_r8*spe_nox,  ncol, lchnk ) ! N(4S) produciton (molec/cm3/s)
-       call outfld( 'OH_SPE' ,        spe_hox,  ncol, lchnk ) ! HOX produciton (molec/cm3/s)
-
-       extfrc(:ncol,:pver,n2d_ndx) = extfrc(:ncol,:pver,n2d_ndx) +  0.7_r8*spe_nox(:ncol,:pver)
-       extfrc(:ncol,:pver,  n_ndx) = extfrc(:ncol,:pver,  n_ndx) + 0.55_r8*spe_nox(:ncol,:pver)
-       extfrc(:ncol,:pver, oh_ndx) = extfrc(:ncol,:pver, oh_ndx)         + spe_hox(:ncol,:pver)
-
-       call outfld( 'P_Op',  extfrc(:,:,op_ndx), ncol, lchnk )
-       call outfld( 'P_O2p', extfrc(:,:,o2p_ndx), ncol, lchnk )
-       call outfld( 'P_Np',  extfrc(:,:,np_ndx), ncol, lchnk )
-       call outfld( 'P_N2p', extfrc(:,:,n2p_ndx), ncol, lchnk )
-       call outfld( 'P_IONS',extfrc(:,:,n2d_ndx), ncol, lchnk )
-
-    endif
 
 
   end subroutine setext
